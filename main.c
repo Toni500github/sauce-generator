08a46ab6f67b2bba5f08051d3e71ecb361e5b80f
Toni500git <Toni500minecraft@gmail.com>
2022-12-13 08:24:37 +0100

    randomize by time

--- main.c
+++ main.c
@@ -7,13 +7,15 @@ int main (int argc, char *argv[])
 {
   int min = 11125;
   int max = 405238;
-  
+  srand(time(0));
+
   printf("%s\n", "printing random sauce, press CTRL+C to stop");
-  printf("%s\n", "DISCLAIMER: SOME NUMBERS ARE DANGEROUS, JUST CLOSE THESE IMMEDIATLY BY CTRL+W ON BROWSER");
-    sleep(3);
-  while (1) {
-    int r = rand() % (max + 1 - min) + min;
-    printf("\nhttps://nhentai.net/g/%d", r);
+  //DISCLAIMER: SOME NUMBERS ARE DANGEROUS, JUST CLOSE THESE IMMEDIATLY BY CTRL+W ON BROWSER
+    sleep(2);
+  
+    while (1) {
+    int sauce = rand() % (max + 1 - min) + min;
+    printf("\nhttps://nhentai.net/g/%d", sauce);
     usleep(31000);
   };
   return 0;
